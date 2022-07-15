3c7ce42c5729d3ad95d163550564ad7ae22cb8c4
eduardoaugusto10 <eduardoaugusto10@gmail.com>
2022-07-15 17:49:22 -0300

    nova alteração teste

--- project/teste.py
+++ project/teste.py
@@ -9,5 +9,5 @@ def trocar_de_canal(self, aumentar, diminuir):
 controle_remoto = ControleRemoto("preto", "18cm", "5cm")
 trocar_de_canal = ControleRemoto(aumentar, diminuir)
 print(controle_remoto.altura, "cor", controle_remoto.cor)
-
+#testando
 
