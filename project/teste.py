41fdba0ab323042e34db4430f3929ff6348a5904
eduardoaugusto10 <eduardoaugusto10@gmail.com>
2022-07-15 17:45:20 -0300

    git commit -m "mensagem"

---
+++ project/teste.py
@@ -0,0 +1,13 @@
+class ControleRemoto:
+    def __init__(self, cor, altura, profundidade):
+        self.cor = cor
+        self.altura = altura
+        self.profundidade = profundidade
+        
+def trocar_de_canal(self, aumentar, diminuir):
+    
+controle_remoto = ControleRemoto("preto", "18cm", "5cm")
+trocar_de_canal = ControleRemoto(aumentar, diminuir)
+print(controle_remoto.altura, "cor", controle_remoto.cor)
+
+
